c0963ba99afe4ccafd929a0b02df78639df593e1
face <1195509693@qq.com>
2026-07-28 13:55:08 +0800

    测试提交1

--- Assets/jiaoBen/WaypointFollower.cs
+++ Assets/jiaoBen/WaypointFollower.cs
@@ -8,13 +8,11 @@ public class WaypointFollower : MonoBehaviour
     [SerializeField] private float speed = 2f;
     private int currentWaypointIndex = 0;
 
-    // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (Vector2.Distance(waypoints[currentWaypointIndex].transform.position,transform.position) < .1f)
